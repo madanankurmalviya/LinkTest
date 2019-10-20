06ecca4195d3cdc49db283f01fb8b1dddb762e21
ANKUR MALVIYA <56480932+madanankurmalviya@users.noreply.github.com>
2019-10-20 13:18:39 +0530

    LinkTest

---
+++ ecllipse Second/LinkTest/src/test/java/com/freeCrm/Test/LlinkTest.java
@@ -0,0 +1,22 @@
+package com.freeCrm.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LlinkTest 
+{
+	@Test
+	public void linkTestPage()
+	{
+		System.setProperty("webdriver.chrome.driver","E:/ANKUR MALVIYA/eclipse/ecllipse Second/Practise/DriverAnkur/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.google.com");
+		driver.close();
+	}
+
+}
